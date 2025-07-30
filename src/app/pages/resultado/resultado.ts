46d8934e90e2a860eb4e3a7cff1f370495c04282
Odevilma <odevilma74@gmail.com>
2025-07-29 23:05:52 -0300

    commit inicial

--- src/app/pages/resultado/resultado.ts
+++ src/app/pages/resultado/resultado.ts
@@ -1,22 +1,36 @@
-<section class="resultado-section-bronco">
-  <div class="resultado-container-bronco">
-    <h1 class="titulo-resultado">{{ tituloResultado }}</h1>
-    <p class="texto-resultado">{{ textoResultado }}</p>
 
-    <div class="carro-display">
-      <img [src]="imagemCarroAtual" alt="Ford Bronco Sport" class="img-bronco-resultado">
-      <div class="cores-opcoes">
-        <div class="cor-bolinha" [class.selected]="corSelecionada === 'branco'" style="background-color: #fff;" (click)="mudarCor('branco')" title="Branco"></div>
-        <div class="cor-bolinha" [class.selected]="corSelecionada === 'cinza'" style="background-color: #808080;" (click)="mudarCor('cinza')" title="Cinza"></div>
-        <div class="cor-bolinha" [class.selected]="corSelecionada === 'vermelho'" style="background-color: #FF0000;" (click)="mudarCor('vermelho')" title="Vermelho"></div>
-        <div class="cor-bolinha" [class.selected]="corSelecionada === 'azul'" style="background-color: #0000FF;" (click)="mudarCor('azul')" title="Azul"></div>
-        <div class="cor-bolinha" [class.selected]="corSelecionada === 'verde'" style="background-color: #008000;" (click)="mudarCor('verde')" title="Verde"></div>
-        </div>
-    </div>
 
-    <div class="botoes-acao">
-      <a routerLink="/" class="btn btn-voltar-quiz">RefazerTeste</a>
-      <a routerLink="/contato" class="btn btn-agendar-test-drive">Agendar Test Drive</a>
-    </div>
-  </div>
-</section>
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resultado',
+  templateUrl: './resultado.html',
+  styleUrls: ['./resultado.css']
+})
+export class ResultadoComponent {
+  tituloResultado: string = 'Seu resultado';
+  textoResultado: string = 'Parabéns! Aqui está o seu Ford Bronco Sport.';
+  imagemCarroAtual: string = 'assets/img/bronco-branco.png';
+  corSelecionada: string = 'branco';
+
+  mudarCor(cor: string) {
+    this.corSelecionada = cor;
+    switch (cor) {
+      case 'branco':
+        this.imagemCarroAtual = 'assets/img/bronco-branco.png';
+        break;
+      case 'cinza':
+        this.imagemCarroAtual = 'assets/img/bronco-cinza.png';
+        break;
+      case 'vermelho':
+        this.imagemCarroAtual = 'assets/img/bronco-vermelho.png';
+        break;
+      case 'azul':
+        this.imagemCarroAtual = 'assets/img/bronco-azul.png';
+        break;
+      case 'verde':
+        this.imagemCarroAtual = 'assets/img/bronco-verde.png';
+        break;
+    }
+  }
+}
